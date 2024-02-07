ebb65e2968d0cffe9e62260313ecd501876faf54
asiahaws1 <144909095+asiahaws1@users.noreply.github.com>
2024-02-06 21:36:12 -0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts)",
-"./index.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ["./src/**/*.{html,ts}",
+  "./index.html"],
+    theme: {
+      extend: {},
+    },
+    plugins: [require("daisyui")],
+    daisyui: {
+      themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula"]
+    }
+  }
