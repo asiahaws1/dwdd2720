56c0eba5b60166bab22d7c2566fa5417299b4a55
asiahaws1 <144909095+asiahaws1@users.noreply.github.com>
2024-03-28 19:46:13 -0600

    put in the examplecode snippet

---
+++ src/lib/classes/ExampleCodeSnippet.ts
@@ -0,0 +1,22 @@
+import { ExampleCode } from "../../types/ExampleCode"
+import hljs from "highlight.js/lib/core";
+import typescript from "highlight.js/lib/languages/typescript";
+
+hljs.registerLanguage("typescript", typescript);
+
+export class ExampleCodeSnippet implements ExampleCode {
+  constructor(
+    public title: string,
+    public explanation: string,
+    public code: string
+  ) {}
+  renderCodeSnippet(): string {
+    return `<h3 class="font-bold text-black bg-slate-400 rounded p-1 my-4 text-center">
+    ${this.title}
+    </h3>
+    <p class="text-white">${this.explanation}</p>
+    <pre>
+    <code>${hljs.highlight(this.code, { language: "typescript" }).value}</code>
+    </pre>`
+  }
+}
